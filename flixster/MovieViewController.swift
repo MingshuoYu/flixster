eeecac1d5fcfd9cb440c99c1913c129ed4b0373b
MingshuoYu <helenmingshuo918@gmail.com>
2021-02-22 00:40:47 -0800

    add dependency with pictures

--- flixster/MovieViewController.swift
+++ flixster/MovieViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieViewController: UIViewController, UITableViewDataSource,UITableViewDelegate{
     
@@ -57,6 +58,12 @@ class MovieViewController: UIViewController, UITableViewDataSource,UITableViewDe
         let synopsis = movie["overview"] as! String
         cell.titleLabel!.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterUrl!)
         return cell
     }
     
